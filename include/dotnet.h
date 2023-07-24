4ca1232508eeba769b7917e2062c18902d5751a3
星梦 <starsdream00@icloud.com>
2023-07-24 13:36:36 +0800

    fix: Fix build

--- include/dotnet.h
+++ include/dotnet.h
@@ -3,15 +3,13 @@
 
 #include <main.h>
 
-load_assembly_and_get_function_pointer_fn
+extern load_assembly_and_get_function_pointer_fn
     load_assembly_and_get_function_pointer_fptr;
-// get_function_pointer_fn get_function_pointer_fptr;
-load_assembly_fn load_assembly_fptr;
-// load_assembly_bytes_fn load_assembly_bytes_fptr;
+extern load_assembly_fn load_assembly_fptr;
 
-hostfxr_initialize_for_runtime_config_fn init_fptr;
-hostfxr_get_runtime_delegate_fn get_delegate_fptr;
-hostfxr_close_fn close_fptr;
+extern hostfxr_initialize_for_runtime_config_fn init_fptr;
+extern hostfxr_get_runtime_delegate_fn get_delegate_fptr;
+extern hostfxr_close_fn close_fptr;
 
 int load_hostfxr();
 
