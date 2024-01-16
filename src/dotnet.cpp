bb0eaf9627dafb867dac10aa71c7487cc96b65d1
予纾 <starsdream00@icloud.com>
2024-01-16 23:39:39 +0800

    fix: Fix loading of plugin management

--- src/dotnet.cpp
+++ src/dotnet.cpp
@@ -11,28 +11,28 @@ hostfxr_close_fn close_fptr;
 int load_hostfxr() {
     char_t buffer[MAX_PATH];
     size_t buffer_size = sizeof(buffer) / sizeof(char_t);
-    int rc = get_hostfxr_path(buffer, &buffer_size, NULL);
+    int rc = get_hostfxr_path(buffer, &buffer_size, nullptr);
     if (rc != 0) {
         return rc;
     }
     HMODULE lib = ::LoadLibraryW(buffer);
-    if (lib == NULL) {
+    if (lib == nullptr) {
         return -1;
     }
     init_fptr = (hostfxr_initialize_for_runtime_config_fn)::GetProcAddress(
         lib, "hostfxr_initialize_for_runtime_config");
-    if (init_fptr == NULL) {
+    if (init_fptr == nullptr) {
         ::FreeLibrary(lib);
         return -1;
     }
     get_delegate_fptr = (hostfxr_get_runtime_delegate_fn)::GetProcAddress(
         lib, "hostfxr_get_runtime_delegate");
-    if (get_delegate_fptr == NULL) {
+    if (get_delegate_fptr == nullptr) {
         ::FreeLibrary(lib);
         return -1;
     }
     close_fptr = (hostfxr_close_fn)::GetProcAddress(lib, "hostfxr_close");
-    if (close_fptr == NULL) {
+    if (close_fptr == nullptr) {
         ::FreeLibrary(lib);
         return -1;
     }
@@ -40,21 +40,21 @@ int load_hostfxr() {
 }
 
 int init_delegate_fptrs(const char_t *config_path) {
-    hostfxr_handle cxt = NULL;
-    int rc = init_fptr(config_path, NULL, &cxt);
-    if (rc != 0 || cxt == NULL) {
+    hostfxr_handle cxt = nullptr;
+    int rc = init_fptr(config_path, nullptr, &cxt);
+    if (rc != 0 || cxt == nullptr) {
         close_fptr(cxt);
         return rc;
     }
     rc =
         get_delegate_fptr(cxt, hdt_load_assembly, (void **)&load_assembly_fptr);
-    if (rc != 0 || load_assembly_fptr == NULL) {
+    if (rc != 0 || load_assembly_fptr == nullptr) {
         close_fptr(cxt);
         return rc;
     }
     rc = get_delegate_fptr(cxt, hdt_get_function_pointer,
                            (void **)&get_function_pointer_fptr);
-    if (rc != 0 || get_function_pointer_fptr == NULL) {
+    if (rc != 0 || get_function_pointer_fptr == nullptr) {
         close_fptr(cxt);
         return rc;
     }
--- src/main.cpp
+++ src/main.cpp
@@ -3,42 +3,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <Windows.h>
+#include <filesystem>
 
 void init_loading(ll::Logger& logger) {
     int rc = load_hostfxr();
     if (rc != 0) {
-        logger.info("Failed to load hostfxr: 0x{:x}\n", rc);
+        logger.info("Failed to load hostfxr: {}\n", rc);
         return;
     }
     rc = init_delegate_fptrs(LIBRARY_DIR_PATH MAIN_NAMESPACE
                              L"." PLUGIN_MANAGEMENT_NAME L".runtimeconfig.json");
     if (rc != 0) {
-        logger.info("Failed to get functions from hostfxr: 0x{:x}\n", rc);
+        logger.info("Failed to get functions from hostfxr: {}\n", rc);
         return;
     }
-    char_t plugin_management_path[MAX_PATH];
-    DWORD size = ::GetFullPathNameW(LIBRARY_DIR_PATH, sizeof(plugin_management_path) / sizeof(char_t), plugin_management_path, nullptr);
-    if (size == 0) {
-        logger.info("Failed to get the path of the plugin management library: 0x{:x}\n", rc);
-        return;
-    }
-    strcat(strcat((char *)plugin_management_path, "\\"),
-           MAIN_NAMESPACE "." PLUGIN_MANAGEMENT_NAME ".dll");
-    rc = load_assembly_fptr(plugin_management_path, NULL, NULL);
+    std::filesystem::path plugin_management_path =
+        std::filesystem::absolute(LIBRARY_DIR_PATH MAIN_NAMESPACE "."
+                                  PLUGIN_MANAGEMENT_NAME ".dll");
+    rc = load_assembly_fptr(plugin_management_path.c_str(), nullptr, nullptr);
     if (rc != 0) {
-        logger.info("Failed to load plugin management: 0x{:x}\n", rc);
+        logger.info("Failed to load plugin management: {}\n", rc);
         return;
     }
     typedef void(CORECLR_DELEGATE_CALLTYPE * entry_point_fn)();
-    entry_point_fn entry_point = NULL;
+    entry_point_fn entry_point = nullptr;
     rc = get_function_pointer_fptr(
         MAIN_NAMESPACE L"." PLUGIN_MANAGEMENT_NAMESPACE L".Main, " MAIN_NAMESPACE
                        L"." PLUGIN_MANAGEMENT_NAME,
-        L"Initialize", UNMANAGEDCALLERSONLY_METHOD, NULL, NULL,
+        L"Initialize", UNMANAGEDCALLERSONLY_METHOD, nullptr, nullptr,
         (void **)&entry_point);
     if (rc != 0) {
-        logger.info("Failed to get entry point from plugin management: 0x{:x}\n", rc);
+        logger.info("Failed to get entry point from plugin management: {}\n", rc);
         return;
     }
     entry_point();
